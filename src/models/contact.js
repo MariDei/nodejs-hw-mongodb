ac7f0ec3c534a647f2a118ca415b707065e917e8
Marianna Dei <d.marianna@icloud.com>
2024-07-28 01:42:07 +0300

    Created contact.js

---
+++ src/models/contact.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+export { Contact };
