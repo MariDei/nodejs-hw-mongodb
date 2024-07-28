879f3772deb9736dcaa5bac0a148b609bca2d392
Marianna Dei <d.marianna@icloud.com>
2024-07-28 23:25:30 +0300

    Created GET routers

--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import { getAllContacts, getContactByID } from './services/contacts.js';
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
-
+const PORT = Number(env('PORT', '3000'));
 export const startServer = () => {
   const app = express();
-
-  app.use(express());
+  app.use(express.json());
   app.use(cors());
-
   app.use(
     pino({
       transport: {
@@ -20,18 +17,50 @@ export const startServer = () => {
     }),
   );
 
-  app.use('*', (req, res) => {
-    res.status(404).send({
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+      res.status(200).json({
+        data: contacts,
+        message: 'Successfully found contacts!',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactByID(contactId);
+
+      if (!contact) {
+        return res.status(404).json({
+          message: 'Contact not found',
+        });
+      }
+
+      res.status(200).json({
+        data: contact,
+        message: `Successfully found contact with id ${contactId}!`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
       message: 'Not found',
     });
   });
 
-  //   app.use((err, req, res) => {
-  //     res.status(500).send({
-  //       message: 'Something went wrong',
-  //       error: err.message,
-  //     });
-  //   });
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ src/services/contacts.js
@@ -0,0 +1,17 @@
+import { Contact } from '../models/contact.js';
+
+export const getAllContacts = async () => {
+  try {
+    const contacts = await Contact.find();
+    console.log('Contacts:', contacts);
+    return contacts;
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+    throw error;
+  }
+};
+
+export const getContactById = async (contactId) => {
+  const contact = await Contact.findById(contactId);
+  return contact;
+};
