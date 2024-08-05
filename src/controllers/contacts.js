0f17afa707fc03e0c4118800658f52982f0f1488
Marianna Dei <d.marianna@icloud.com>
2024-08-05 16:40:21 +0300

    Created PATCH route

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,6 +1,10 @@
 import createHttpError from 'http-errors';
-import { getAllContacts, getContactById } from './services/contacts.js';
-import { createContact } from '../services/contacts.js';
+import {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+} from './services/contacts.js';
 
 export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
@@ -32,5 +36,21 @@ export const createContactController = async (req, res) => {
     status: 201,
     message: `Successfully created a contact!`,
     data: contact,
+  });
+};
+
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully patched a contact!`,
+    data: result.contact,
   });
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,6 +4,7 @@ import {
   getContactsController,
   getContactsByIdController,
   createContactController,
+  patchContactController,
 } from '../controllers/contacts';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper';
@@ -15,5 +16,7 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 
 router.post('/contacts', ctrlWrapper(createContactController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 
 export default router;
