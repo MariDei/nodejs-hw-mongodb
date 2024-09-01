7bfdc2f31335563a1b3405e8bf1a01f8388a0e51
Marianna Dei <d.marianna@icloud.com>
2024-09-01 19:33:36 +0300

    Created route POST /auth/reset-pwd

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -6,6 +6,7 @@ import {
 } from '../services/auth.js';
 import { THIRTY_DAYS } from '../constants/index.js';
 import { requestResetToken } from '../services/auth.js';
+import { resetPassword } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const payload = {
@@ -84,9 +85,18 @@ export const refreshUserSessionController = async (req, res) => {
 
 export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
-  res.json({
+  res.status(200).json({
     status: 200,
     message: 'Reset password email has been successfully sent.',
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+  res.status(200).json({
+    message: 'Password was successfully reset!',
+    status: 200,
     data: {},
   });
 };
