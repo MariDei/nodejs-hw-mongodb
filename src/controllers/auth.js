2e42ad58b5d29462314cc3af352d2f767a51ddf2
Marianna Dei <d.marianna@icloud.com>
2024-09-04 15:17:43 +0300

    Fixed errors

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -87,7 +87,7 @@ export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.status(200).json({
     status: 200,
-    message: 'Reset password email has been successfully sent.',
+    message: 'Reset password email has been successfully sent!',
     data: {},
   });
 };
@@ -95,7 +95,7 @@ export const requestResetEmailController = async (req, res) => {
 export const resetPasswordController = async (req, res) => {
   await resetPassword(req.body);
   res.status(200).json({
-    message: 'Password was successfully reset!',
+    message: 'Password has been successfully reset!',
     status: 200,
     data: {},
   });
